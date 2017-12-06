4f8843cfdc2e0194e98c41bd32da030c976f1159
Ruiz, Gerald <gerald.ruiz@accenture.com>
2017-12-06 16:11:54 -0600

    checked

--- src/components/Mapper.js
+++ src/components/Mapper.js
@@ -24,21 +24,33 @@ class Mapper extends Component {
   render() {
     return (
       <div>
-        <Map style={styles.map} center={position} zoom={4}>
+        <Map
+          style={styles.map}
+          center={position}
+          zoom={4}
+        >
           <LayersControl position="topright">
-            <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
+
+
+            <LayersControl.BaseLayer checked name="OpenStreetMap.BlackAndWhite">
               <TileLayer
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
               />
             </LayersControl.BaseLayer>
+
+
             <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
               <TileLayer
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
             </LayersControl.BaseLayer>
-            <LayersControl.Overlay name="Marker with popup">
+
+
+            <LayersControl.Overlay
+              name="Marker with popup"
+            >
               <Marker position={[29.761993, -95.366302]}>
                 <Popup>
                   <span>
@@ -47,14 +59,26 @@ class Mapper extends Component {
                 </Popup>
               </Marker>
             </LayersControl.Overlay>
-            <LayersControl.Overlay name="Feature group">
-              <FeatureGroup color="purple">
-                <Popup>
-                  <span>Popup in FeatureGroup</span>
-                </Popup>
-                <Circle center={[29.761993, -95.366302]} radius={200} />
+
+
+            <LayersControl.Overlay
+              name="Feature group"
+            >
+              <FeatureGroup
+                color="purple"
+              >
+                  <Popup>
+                    <span>Popup in FeatureGroup</span>
+                  </Popup>
+
+                  <Circle
+                    center={[29.761993, -95.366302]}
+                    radius={200}
+                  />
               </FeatureGroup>
             </LayersControl.Overlay>
+
+
           </LayersControl>
         </Map>
       </div>
