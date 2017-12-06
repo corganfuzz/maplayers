36a8d0d9919e99f65456d8898b8aefd935f0cd5d
Ruiz, Gerald <gerald.ruiz@accenture.com>
2017-12-06 16:03:12 -0600

    layers in place

--- src/components/Mapper.js
+++ src/components/Mapper.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import Layzs from "./Layz";
+// import { render } from "react-dom";
+// import Layzs from "./Layz";
+import {
+  Map,
+  LayersControl,
+  TileLayer,
+  Marker,
+  Popup,
+  FeatureGroup,
+  Circle
+} from "react-leaflet";
 
 const position = [29.761993, -95.366302];
 const styles = {
@@ -16,25 +24,44 @@ class Mapper extends Component {
   render() {
     return (
       <div>
-        <Map
-          style={styles.map}
-          center={position}
-          zoom={4}>
-          <TileLayer
-            url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
-            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          />
-          <Marker position={position}>
-            <Popup>
-              <span>
-                I'm here bro <br /> Yup.
-              </span>
-            </Popup>
-          </Marker>
+        <Map style={styles.map} center={position} zoom={4}>
+          <LayersControl position="topright">
+            <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
+              <TileLayer
+                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
+              <TileLayer
+                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.Overlay name="Marker with popup">
+              <Marker position={[29.761993, -95.366302]}>
+                <Popup>
+                  <span>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </span>
+                </Popup>
+              </Marker>
+            </LayersControl.Overlay>
+            <LayersControl.Overlay name="Feature group">
+              <FeatureGroup color="purple">
+                <Popup>
+                  <span>Popup in FeatureGroup</span>
+                </Popup>
+                <Circle center={[29.761993, -95.366302]} radius={200} />
+              </FeatureGroup>
+            </LayersControl.Overlay>
+          </LayersControl>
         </Map>
-        <Layzs />
       </div>
     );
   }
 }
+
 export default Mapper;
+
+// url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
